c5fa1abe796258e0926f25f944a77f5e294a4134
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-10-16 12:51:20 +0200

    f

--- Skitgubbe/Assets/!Bell/Scripts/Card.cs
+++ Skitgubbe/Assets/!Bell/Scripts/Card.cs
@@ -7,6 +7,14 @@ public class Card : MonoBehaviour
     [SerializeField] int cardNumber;
     [SerializeField] bool isDead = false;
 
+    [SerializeField] GameObject grabObject;
+
+
+    public void GrabObject(bool grabObjectOn)
+    {
+        grabObject.SetActive(grabObjectOn);
+    }
+
     public int GetCardNum()
     {
         return cardNumber;
