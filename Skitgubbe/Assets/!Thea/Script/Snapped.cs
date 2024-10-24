06414b9af01ab39e5f8e2c08a4123f6265fa5c43
Telonisse <161740040+Telonisse@users.noreply.github.com>
2024-10-24 11:20:50 +0200

    D

--- Skitgubbe/Assets/!Thea/Script/Snapped.cs
+++ Skitgubbe/Assets/!Thea/Script/Snapped.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Snapped : MonoBehaviour
 {
     public bool isSnappedBool;
+    public MeshRenderer childMeshRenderer;
     public void IsSnapped()
     {
         isSnappedBool = true;
+
+        if (childMeshRenderer != null)
+        {
+            childMeshRenderer.enabled = false;
+        }
     }
 
     public void IsNotSnapped()
     {
         isSnappedBool = false;
+
+        if (childMeshRenderer != null)
+        {
+            childMeshRenderer.enabled = true;
+        }
     }
 }
